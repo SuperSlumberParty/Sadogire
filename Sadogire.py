f3813447570befb4885e9c63c2d26fdf6415cd32
Kaname <juno@kamikaze.careers>
2021-10-27 11:54:54 +0200

    Initial commit

---
+++ Sadogire.py
@@ -0,0 +1,53 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import Bot, has_permissions, CheckFailure
+from Config import *
+
+import zmq
+import json
+
+
+# Server block start
+async def Init():
+    sock = zmq.Context().socket(zmq.REP)
+    sock.bind(f"tcp://*:{INPORT}")
+    while True:
+        reply = await Process(json.loads(sock.recv_string()))
+        sock.send_string(reply)
+
+async def Process(message):
+    if (type(message) == list):
+        if (message[0] != SECRET):
+            await ActionLog("Wrong key inputted.")
+            return "Wrong key!"
+        await ActionLog("Got a message from a node")
+        return "OK!"
+    else:
+        return "418 I'm a teapot"
+# Server block end
+
+# Bot preconfiguration
+Triton=commands.Bot(command_prefix="<")
+
+async def ActionLog(message):
+    await Triton.get_channel(LOGCHANNEL).send(message)
+
+# Init event - starts when the bot is being booted up
+@Triton.event
+async def on_ready():
+    print("Sadogire is running!")
+    asyncio.get_event_loop().create_task(Init())
+    await ActionLog("Triton instance launched.")
+
+
+def Boot():
+    if (TOKEN == ""):
+        print("You have not configured the config file!\nPlease edit config.py with the required variables!")
+        exit(0)
+    else:
+        if (SECRET == "TwinklingStar"):
+            print("You are running Sadogire with the default secret! This is unsecure and may grant access to any third party!")
+        Triton.run(TOKEN)
+
+Boot()
